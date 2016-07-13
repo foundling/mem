c9ddc5f2b4e12d32e6eaa0cda76efe04646604c2
Alex Ramsdell <alexramsdell@gmail.com>
2016-07-12 22:39:39 -0700

    add version number, add types to add callback parameters

--- mem.py
+++ mem.py
@@ -8,15 +8,16 @@ DB_PATH = config['db']
 db = sqlite3.connect(DB_PATH)
 
 
+@click.version_option(1.0)
 @click.group()
 def cli():
     pass
 
 @cli.command()
 @click.argument('name')
-@click.argument('description', default='')
-@click.argument('created', default='')
-@click.argument('complete', default='')
+@click.argument('description', default='', type=click.STRING)
+@click.argument('created', default='now', type=click.STRING)
+@click.argument('complete', default=0, type=click.INT)
 def add(name, description, created, complete):
     click.echo('Adding task %s' % name)
 
